a6963057e84d55fb44e6df353d21c4cd29fde98e
md saif <mohdsaif9170613062@gmail.com>
2023-02-13 01:59:48 +0530

    Update branch and pushed

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Header;
