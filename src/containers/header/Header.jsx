11316bd66bde81c331ee9479968d3b94fa040540
MD SAIF <98917571+12saif@users.noreply.github.com>
2023-02-14 00:50:29 +0530

    update header componentes (#9)
    Co-authored-by: md saif <mohdsaif9170613062@gmail.com>

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import "./header.css";
+import peopleGroup from "../../assets/Group 81.png";
+import headerIllustration from "../../assets/Header Illustration.png";
 
 const Header = () => {
-<<<<<<< HEAD
   return (
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let's Build Something amazing with GPT-3 OpenAI
         </h1>
-        
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={peopleGroup} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        <div className="gpt__header-img">
+          <img src={headerIllustration} alt="AI" />
+        </div>
       </div>
     </div>
   );
-=======
-  return <div>Header</div>;
->>>>>>> ad588c514726cca38abc7e75212bcba923364221
 };
 
 export default Header;
