907742ae2062ea617b19176eb0be4bc89433fb3c
md saif <mohdsaif9170613062@gmail.com>
2023-02-17 21:50:39 +0530

    update whatgpt3 components and add feature components

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./whatGPT3.css";
-import Feature from './../../components/feature/Feature';
+import Feature from "./../../components/feature/Feature";
 
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgpt3 section__padding" id="whpt3">
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
 
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">
+        <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
         <p>Explore The Library</p>
