31448e2c6527da94db952460666040c3f3f38d44
md saif <mohdsaif9170613062@gmail.com>
2023-02-21 22:13:41 +0530

    update feture commponents and complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "./App.css";
-import Footer from "./containers/footer/Footer";
-import Blog from "./containers/blog/Blog";
-import Possibility from "./containers/possibility/Possibility";
-import Feature from "./components/feature/Feature";
-import WhatGPT3 from "./containers/whatGPT3/WhatGPT3";
-import Header from "./containers/header/Header";
-// import {
-  //   Footer,
-  //   Blog,
-  //   Possibility,
-  //   Feature,
-  //   WhatGPT3,
-  //   Header,
-  // } from "./containers";
-  import { CTA, Brand, Navbar } from "./components";
-  
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+
+import { CTA, Brand, Navbar } from "./components";
 
 const App = () => {
   return (
@@ -26,7 +21,7 @@ const App = () => {
       </div>
       <Brand />
       <WhatGPT3 />
-      <Feature />
+      <Features />
       <Possibility />
       <CTA />
       <Blog />
