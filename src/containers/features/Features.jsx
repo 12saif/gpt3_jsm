336fecda68d7b64b919649b8c745b9d48550b6b5
MD SAIF <98917571+12saif@users.noreply.github.com>
2023-02-22 23:21:41 +0530

    Update feature components 2 (#14)
    * update header componentes
    
    * create brand and whatgpt3 components
    
    * update whatgpt3 components and add feature components
    
    * upadate feature componets title text
    
    * update feture commponents and complete
    
    * update components
    
    ---------
    
    Co-authored-by: md saif <mohdsaif9170613062@gmail.com>

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -20,6 +20,7 @@ const featuresData = [
     text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..",
   },
 ];
+// !  Feture data create here top
 
 const Features = () => {
   return (
