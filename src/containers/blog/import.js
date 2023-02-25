372eed80abc51cf0a4caf29889021f613fac6a52
md saif <mohdsaif9170613062@gmail.com>
2023-02-25 22:51:02 +0530

    Update and Add BLOG,ARTICLE Components

---
+++ src/containers/blog/import.js
@@ -0,0 +1,7 @@
+import blog01 from "../../assets/Rectangle 22-4.png";
+import blog02 from "../../assets/Rectangle 22-1.png";
+import blog03 from "../../assets/Rectangle 22-2.png";
+import blog04 from "../../assets/Rectangle 22-3.png";
+import blog05 from "../../assets/Rectangle 22.png";
+
+export { blog01, blog02, blog03, blog04, blog05 };
